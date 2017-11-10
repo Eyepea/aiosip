cbc94311219ca7edea6f86fcc429dd7f6e4bc894
Ludovic Gasc (GMLudo) <gmludo@gmail.com>
2017-11-10 15:44:24 +0100

    Remove old trove classifiers for Python 3.3 and 3.4

--- setup.py
+++ setup.py
@@ -45,8 +45,6 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Topic :: Communications',
