ef41fff60529c80614d3226b628808cf5920fb2b
Jukka Vaisanen <vaizki@vaizki.fi>
2019-02-08 11:16:36 +0200

    Added copying of Record-Route: headers to responses Fixed Via: parsing if no port was given Fixed Via.from_header() to error out correctly Added exceptions to abstract DialogBase methods

--- aiosip/via.py
+++ aiosip/via.py
@@ -27,7 +27,9 @@ class Via(MutableMapping):
         if not isinstance(params, Param):
             self._via['params'] = Param(self._via['params'])
 
-        self._via['host'], self._via['port'] = self._via['sentby'].rsplit(':', 1)
+        sentby_parts = self._via['sentby'].rsplit(':', 1)
+        self._via['host'] = sentby_parts[0]
+        self._via['port'] = sentby_parts[1] if len(sentby_parts) > 1 else "5060"
 
     @classmethod
     def from_header(cls, via):
@@ -35,8 +37,7 @@ class Via(MutableMapping):
             m = s.match(via)
             if m:
                 return cls(m.groupdict())
-        else:
-            raise ValueError('Not valid via address')
+        raise ValueError('Not valid via address')
 
     # MutableMapping API
     def __eq__(self, other):
